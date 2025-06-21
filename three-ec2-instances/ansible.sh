036e7c75c16cf3bda634e70d8f64834ffba23559
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-06-21 23:48:02 +0530

    expense ansible roles common

--- three-ec2-instances/ansible.sh
+++ three-ec2-instances/ansible.sh
@@ -3,6 +3,11 @@ dnf install ansible -y
 cd /tmp
 git clone https://github.com/Lingaiahthammisetti/4.7.expense-ansible-roles-common.git
 cd 4.7.expense-ansible-roles-common
+# ansible-playbook -i inventory.ini db.yaml
+# ansible-playbook -i inventory.ini db.yaml #Run twice db.yaml
+# ansible-playbook -i inventory.ini backend.yaml
+# ansible-playbook -i inventory.ini frontend.yaml
+
 ansible-playbook main.yaml -e component=db
 ansible-playbook main.yaml -e component=db #Run twice db.yaml
 ansible-playbook main.yaml -e component=backend
